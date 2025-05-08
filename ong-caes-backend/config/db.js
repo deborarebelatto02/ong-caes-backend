a62ad1222c3b1abb67409bd6c4617d8916fcbcd0
deborarebelatto02 <167138147+deborarebelatto02@users.noreply.github.com>
2025-05-08 17:46:34 -0300

    atualizando arquivos

--- ong-caes-backend/config/db.js
+++ ong-caes-backend/config/db.js
@@ -7,7 +7,14 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // necessário para conexões em nuvem como o Neon
+      }
+    },
+    logging: false,
   }
 );
 
